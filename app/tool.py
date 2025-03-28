bfff6aa2928a3d362c423e18ac10df89392ae7dd
unknown <shahrukh.priv@gmail.com>
2025-03-28 06:47:13 +0600

    Project Done!

---
+++ app/tool.py
@@ -0,0 +1,102 @@
+import os
+from typing import Optional
+
+import serpapi
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain_core.tools import tool
+
+class FlightsInput(BaseModel):
+    departure_airport: Optional[str] = Field(description='Departure airport code (IATA)')
+    arrival_airport: Optional[str] = Field(description='Arrival airport code (IATA)')
+    outbound_date: Optional[str] = Field(description='Parameter defines the outbound date. The format is YYYY-MM-DD. e.g. 2025-05-28')
+    return_date: Optional[str] = Field(description='Parameter defines the return date. The format is YYYY-MM-DD. e.g. 2025-06-28')
+    adults: Optional[int] = Field(1, description='Parameter defines the number of adults. Default to 1.')
+    children: Optional[int] = Field(0, description='Parameter defines the number of children. Default to 0.')
+    infants_in_seat: Optional[int] = Field(0, description='Parameter defines the number of infants in seat. Default to 0.')
+    infants_on_lap: Optional[int] = Field(0, description='Parameter defines the number of infants on lap. Default to 0.')
+
+
+class FlightsInputSchema(BaseModel):
+    params: FlightsInput
+
+
+@tool(args_schema=FlightsInputSchema)
+def flights_finder(params: FlightsInput):
+    '''
+    Find flights using the google flights engine
+
+    Returns:
+        dict: Flight search results
+    
+    '''
+
+    params = {
+        'api_key': os.environ.get('SERP_API_KEY'),
+        'engine': 'google_flights',
+        'hl': 'en',
+        'gl': 'bd',
+        'departure_id': params.departure_airport,
+        'arrival_id': params.arrival_airport,
+        'outbound_date': params.outbound_date,
+        'return_date': params.return_date,
+        'currency': 'BDT',
+        'adults': params.adults,
+        'infants_in_seat': params.infants_in_seat,
+        'stops': '1',
+        'infants_on_lap': params.infants_on_lap,
+        'children': params.children
+    }
+    try:
+        search = serpapi.search(params)
+        results = search.data['best_flights']
+    except Exception as e:
+        results = str(e)
+    return results
+
+
+
+
+class HotelsInput(BaseModel):
+    q: str = Field(description='Location of the hotel')
+    check_in_date: str = Field(description='Check-in date. The format is YYYY-MM-DD. e.g. 2024-06-22')
+    check_out_date: str = Field(description='Check-out date. The format is YYYY-MM-DD. e.g. 2024-06-28')
+    sort_by: Optional[str] = Field(8, description='Parameter is used for sorting the results. Default is sort by highest rating')
+    adults: Optional[int] = Field(1, description='Number of adults. Default to 1.')
+    children: Optional[int] = Field(0, description='Number of children. Default to 0.')
+    rooms: Optional[int] = Field(1, description='Number of rooms. Default to 1.')
+    hotel_class: Optional[str] = Field(
+        None, description='Parameter defines to include only certain hotel class in the results. for example- 2,3,4')
+
+
+class HotelsInputSchema(BaseModel):
+    params: HotelsInput
+
+
+@tool(args_schema=HotelsInputSchema)
+def hotels_finder(params: HotelsInput):
+    '''
+    Find hotels using the Google Hotels engine.
+
+    Returns:
+        dict: Hotel search results.
+    '''
+
+    params = {
+        'api_key': os.environ.get('SERPAPI_API_KEY'),
+        'engine': 'google_hotels',
+        'hl': 'en',
+        'gl': 'bd',
+        'q': params.q,
+        'check_in_date': params.check_in_date,
+        'check_out_date': params.check_out_date,
+        'currency': 'BDT',
+        'adults': params.adults,
+        'children': params.children,
+        'rooms': params.rooms,
+        'sort_by': params.sort_by,
+        'hotel_class': params.hotel_class
+    }
+
+    search = serpapi.search(params)
+    results = search.data
+    return results['properties'][:5]
